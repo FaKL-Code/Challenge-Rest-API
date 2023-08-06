4e953565bd6ffd95b0d622e76ab1a263b6e0f99d
Julio Facal <julio@facal.dev>
2023-08-05 22:52:24 -0300

    fix - DoubleField to FloatField + Migrations

--- challenge/quotegenius/models.py
+++ challenge/quotegenius/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Product(models.Model):
 
-    name = models.CharField(unique=True, null=False)
-    barcode = models.CharField(unique=True, null=False)
+    name = models.CharField(unique=True, null=False, max_length=255)
+    barcode = models.CharField(unique=True, null=False, max_length=255)
 
 class Customer(models.Model):
 
@@ -18,18 +18,18 @@ class SupplierProduct(models.Model):
 
     supplier_id = models.IntegerField(null=False)
     product_id = models.IntegerField(null=False)
-    quantity = models.DoubleField(null=Flase, default=1)
-    price = models.DoubleField(null=False)
+    quantity = models.FloatField(null=False, default=1)
+    price = models.FloatField(null=False)
 
 class Purchase(models.Model):
 
     supplier_id = models.IntegerField(null=False)
     customer_id = models.IntegerField(null=False)
-    total_amount = models.DoubleField(null=False)
+    total_amount = models.FloatField(null=False)
 
 class PurchaseProduct(models.Model):
 
     product_id = models.IntegerField(null=False)
     purchase_id = models.IntegerField(null=False)
-    quantity = models.DoubleField(null=False)
-    price = models.DoubleField(null=False)
+    quantity = models.FloatField(null=False)
+    price = models.FloatField(null=False)
