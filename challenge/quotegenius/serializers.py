c068b86c57062c6b4bc0e031404fce43fa91387e
Julio Facal <julio@facal.dev>
2023-08-05 22:57:18 -0300

    serializers without many to many

---
+++ challenge/quotegenius/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from models import Customer, Product, Purchase, PurchaseProduct, Supplier, SupplierProduct
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('name',)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'barcode')
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = ('supplier_id', 'customer_id', 'total_amount')
+
+class PurchaseProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseProduct
+        fields = ('product_id', 'purchase_id', 'quantity', 'price')
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = ('name', 'email')
+
+class SupplierProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SupplierProduct
+        fields = ('supplier_id', 'product_id', 'quantity', 'price')
