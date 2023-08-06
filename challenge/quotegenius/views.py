58af19ab609d002731830ea24a951a16f86e1efd
Julio Facal <julio@facal.dev>
2023-08-05 23:03:42 -0300

    basic viewsets

--- challenge/quotegenius/views.py
+++ challenge/quotegenius/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import serializers, viewsets
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from serializers import UserSerializer, ProductSerializer, CustomerSerializer, PurchaseProductSerializer, PurchaseProductSerializer, SupplierProductSerializer, SupplierSerializer
+from models import Customer, Product, Purchase, PurchaseProduct, Supplier, SupplierProduct
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class PurchaseViewSet(viewsets.ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseProductSerializer
+
+class PurchaseProduct(viewsets.ModelViewSet):
+    queryset = PurchaseProduct.objects.all()
+    serializer_class = PurchaseProductSerializer
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+class SupplierProductViewSet(viewsets.ModelViewSet):
+    queryset = SupplierProduct.objects.all()
+    serializer_class = SupplierProductSerializer
+
+
