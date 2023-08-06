5661bf823d6c601209d4dbcd9fcbfb118231a2b9
Julio Facal <julio@facal.dev>
2023-08-05 22:48:13 -0300

    models without many to many

--- challenge/quotegenius/models.py
+++ challenge/quotegenius/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(unique=True, null=False)
+    barcode = models.CharField(unique=True, null=False)
+
+class Customer(models.Model):
+
+    name = models.CharField(unique=True, null=False, max_length=50)
+
+class Supplier(models.Model):
+
+    name = models.CharField(unique=True, null=False, max_length=50)
+    email = models.CharField(unique=True, null=False, max_length=50)
+
+class SupplierProduct(models.Model):
+
+    supplier_id = models.IntegerField(null=False)
+    product_id = models.IntegerField(null=False)
+    quantity = models.DoubleField(null=Flase, default=1)
+    price = models.DoubleField(null=False)
+
+class Purchase(models.Model):
+
+    supplier_id = models.IntegerField(null=False)
+    customer_id = models.IntegerField(null=False)
+    total_amount = models.DoubleField(null=False)
+
+class PurchaseProduct(models.Model):
+
+    product_id = models.IntegerField(null=False)
+    purchase_id = models.IntegerField(null=False)
+    quantity = models.DoubleField(null=False)
+    price = models.DoubleField(null=False)
