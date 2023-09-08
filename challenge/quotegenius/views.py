29665ab8b15f6e80cbd61448d842cdd4395e558c
Julio Facal <starlyn.dev@gmail.com>
2023-09-07 23:00:47 -0300

    improving filtering options

--- challenge/quotegenius/views.py
+++ challenge/quotegenius/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from rest_framework import filters
+
 from rest_framework import serializers, viewsets
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -17,36 +19,42 @@ class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['name']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['name']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 class PurchaseViewSet(viewsets.ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['supplier_id', 'customer_id']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['supplier__name', 'customer__name']
 
 class PurchaseProductViewSet(viewsets.ModelViewSet):
     queryset = PurchaseProduct.objects.all()
     serializer_class = PurchaseProductSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['product_id', 'purchase_id']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['product__name', 'purchase__customer__name']
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['name']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 class SupplierProductViewSet(viewsets.ModelViewSet):
     queryset = SupplierProduct.objects.all()
     serializer_class = SupplierProductSerializer
     permission_classes = [IsAuthenticated]
-    filterset_fields = ['product_id', 'supplier_id']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['product__name']
 
 
