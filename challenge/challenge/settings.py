6425f8ac94632638e163feeb6595c5c59836db52
Julio Facal <starlyn.dev@gmail.com>
2023-09-30 13:02:21 -0300

    secret key update

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-=$_6&w1txr&8(07*&g9$(^c8itdty*^5ms)5h+#nlhoo^97@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
